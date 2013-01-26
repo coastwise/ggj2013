65aa5a16614b3290af4880ad2b0852de7251a663
Patrick McKenna <patrick.a.mckenna@gmail.com>
2013-01-26 03:02:52 -0500

    Add buttery smoothing to ship controls

--- Assets/Ship/Scripts/ShipController.cs
+++ Assets/Ship/Scripts/ShipController.cs
@@ -7,11 +7,16 @@ public class ShipController : MonoBehaviour {
 	public float maxVerticalAngle = 40;
 	public float horizontalSpeed = 0.1f;
 	public float verticalSpeed = 0.1f;
+	
+	private float h = 0;
+	private float v = 0;
+	
+	public float smoothing = 0.93f;
 
 	void Update () {
 		
-		float h = Input.GetAxis("Horizontal");
-		float v = Input.GetAxis("Vertical");
+		h = h*smoothing + Input.GetAxis("Horizontal")*(1-smoothing);
+		v = v*smoothing + Input.GetAxis("Vertical")*(1-smoothing);
 		
 		transform.rotation = Quaternion.AngleAxis(h*maxHorizontalAngle, Vector3.up) * Quaternion.AngleAxis(v*maxVerticalAngle, Vector3.right);
 		
