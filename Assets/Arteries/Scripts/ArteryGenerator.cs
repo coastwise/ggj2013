1d0ec83e00b2ae03044dfe5f67adeb39a3dd730d
Patrick McKenna <patrick.a.mckenna@gmail.com>
2013-01-25 23:45:17 -0500

    Add initial artery generator script and test scene

---
+++ Assets/Arteries/Scripts/ArteryGenerator.cs
@@ -0,0 +1,29 @@
+using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+[RequireComponent (typeof(Collider))]
+public class ArteryGenerator : MonoBehaviour {
+	
+	private static int arteryCount = 0;
+	
+	private static Object[] prefabs = Resources.LoadAll("ArteryPrefabs", typeof(GameObject));
+	
+	public List<Transform> branchRoots;
+	
+	void Start () {
+		name = "Artery " + arteryCount;
+		arteryCount = arteryCount + 1;
+	}
+	
+	void OnTriggerEnter (Collider other) {
+		if (other.tag != "Player") return;
+		
+		Debug.Log(name + " generating branches...");
+		foreach (Transform branchRoot in branchRoots) {
+			Object randomPrefab = prefabs[Random.Range(0, prefabs.Length)];
+			GameObject branch = Instantiate(randomPrefab, branchRoot.position, Quaternion.identity) as GameObject;
+		}	
+	}
+	
+}
