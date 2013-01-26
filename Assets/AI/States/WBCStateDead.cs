a37ceee889a818b4f1b4769f4e16d36d77d41ac5
Farzad Afshari <farzad.afshari@wickedroar.com>
2013-01-25 22:31:47 -0500

    Created WBC BT
    WBC = White Blood Cell
    BT = Behavior Tree
    
    Also created finite states for WBC

---
+++ Assets/AI/States/WBCStateDead.cs
@@ -0,0 +1,15 @@
+using UnityEngine;
+using System.Collections;
+
+public class WBCStateDead : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
